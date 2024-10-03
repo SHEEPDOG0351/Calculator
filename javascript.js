7d5e89f5d280e4678139ae06672bc52a4f2c49dd
SHEEPDOG0351 <michaelwork2006@gmail.com>
2024-10-03 10:30:29 -0400

    Added placeholder value in output box, updated some javascript code for the caluclator

--- javascript.js
+++ javascript.js
@@ -1,19 +1,31 @@
 let first_operation = [] //Stores the first part of the operation
 let final_operation = [] //Stores the whole operation and should hold the current calculation
 
+
 function calculation(char) {
     let calculation = 0
     let operation = ''
     if (first_operation.length <= 2) {
         if (typeof char === "string") {
-            // Checks for operators and makes sure its not one of the other equals / enter / delete etc buttons.
             if ((char === '+') || (char === '-') || (char === '/') || (char === '*')) {
+                // Checks for operators and makes sure its not one of the other equals / enter / delete etc buttons.
                 operation = char
             } else if (typeof char === 'number') {
+                // Checks for if char is a number, if so, adds the number to the array
                 first_operation.push(char)
+            } else if (char == 'exponent') {
+                base = first_operation[0]
+                result = base ** 2
+            } else if (char == 'sqrt') {
+                number = first_operation[0]
+                result = Math.sqrt(number)
+            } 
+            else {
+                return final_operation
             }
         }
     } else if (first_operation.length == 2){
+        // Checks for the operation through the use of the operation parameter / variable initalized earlier, with the operator assigned to it duing the respective conditional statement
         switch (operation) {
             case '+':
                 calculation = first_operation[0] + first_operation[1]
@@ -34,6 +46,4 @@ function calculation(char) {
 
         final_operation.push(calculation)
     }
-
-    return final_operation
 }
